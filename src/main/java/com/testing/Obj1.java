98767632952d1573dd501cca673b0d37e88df3a4
admin <>
2022-01-07 21:41:13 +0000

    {/src/main/java/com/testing/Obj1.java}

--- src/main/java/com/testing/Obj1.java
+++ src/main/java/com/testing/Obj1.java
@@ -6,12 +6,23 @@ package com.testing;
 
 public class Obj1 implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Obj1() {
-    }
+	private java.lang.String foo;
 
+	public Obj1() {
+	}
 
+	public java.lang.String getFoo() {
+		return this.foo;
+	}
 
+	public void setFoo(java.lang.String foo) {
+		this.foo = foo;
+	}
+
+	public Obj1(java.lang.String foo) {
+		this.foo = foo;
+	}
 
 }
